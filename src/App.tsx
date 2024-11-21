6ecb7c08fa5e1cd2eba1733cb3542e0537db2353
benjamin.baesu <bbaesu@gmail.com>
2024-11-21 02:01:58 -0800

    Initial commit from template

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{width: '100%', backgroundColor: 'red'}}>Testing</div>
+      <div style={{width: '100%', flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Testing 2</div>
+    </div>
+  );
+}
+
+export default App;
+ 
